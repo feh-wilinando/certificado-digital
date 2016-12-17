74e65478cab5cafed1847cc3668cd0ee9f15c8e8
Fernando Furtado <feh.wilinando@gmail.com>
2016-12-17 14:08:29 -0200

    adicionado mascara condicional para telefone

--- src/main/webapp/assets/js/application.js
+++ src/main/webapp/assets/js/application.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+
+    var telefoneMaskBehavior = function(val){
+        return val.replace(/\D/g, '').startsWith("0800")? '0000 0000-0000': '(00) 0000-0000';
+    };
+
+    var option = {
+        onKeyPress: function(val, e, field, options){
+            field.mask(telefoneMaskBehavior.apply({}, arguments), options);
+        }
+    };
+
     $('.cnpj-mask').mask('00.000.000/0000-00');
     $('.cep-mask').mask('00000-000');
-    $('.telefone-mask').mask('(00) 0000-0000');
+    $('.telefone-mask').mask(telefoneMaskBehavior, option);
     $('.celular-mask').mask('(00) 0 0000-0000');
 
 });
